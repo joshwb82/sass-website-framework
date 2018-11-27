50085ddb3862aa35543f54fc3c02dabdbaefe9db
Joshua Winterbottom <joshua.winterbottom@iris-worldwide.com>
2018-11-27 19:17:51 +0000

    updated version 1

--- README.md
+++ README.md
@@ -1,2 +1,166 @@
-# sass-website-framework
-This is a template to setup a sass web framework
+# SASS WEBSITE FRAMEWORK
+
+
+
+## Getting Started
+
+This is a simple website framework, that is run using: 
+* NPM 
+* GULP
+* SASS
+
+It has the following folder structure once setup:
+
+* SRC folder: for pre-processed files 
+* TMP folder: for the local development server
+* DIST folder: for processed and minified files
+
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+npn gulp 3.9.1
+```
+
+```
+npn gulp-inject 5.0.0
+```
+
+```
+npn gulp-webserver 0.9.1
+```
+
+```
+npn gulp-htmlclean 2.7.22
+```
+
+```
+npn gulp-clean-css 3.10.0
+```
+
+```
+npn gulp-concat 2.6.1
+```
+
+```
+npn gulp-uglify 3.0.1
+```
+
+```
+npn delete
+```
+
+```
+npn gulp-sass 4.0.2
+```
+
+
+
+### Installing
+
+Clone or download the repo
+
+```
+https://github.com/joshwb82/non-sass-website-framework
+```
+
+Install Gulp
+
+```
+npm install gulp --save-dev
+```
+
+Install Gulp inject
+
+```
+npm install gulp-inject --save-dev
+```
+
+Install Gulp web server
+
+```
+npm install gulp-webserver --save-dev
+```
+
+Install Gulp html clean
+
+```
+npm install gulp-htmlclean --save-dev
+```
+
+Install Gulp clean css
+
+```
+npm install gulp-clean-css --save-dev
+```
+
+Install Gulp concat
+
+```
+npm install gulp-concat --save-dev
+```
+
+Install Gulp uglify
+
+```
+npm install gulp-uglify --save-dev
+```
+
+Install gulp delete
+
+```
+npm install del --save-dev
+```
+
+Install gulp sass
+
+```
+npm install node-sass gulp-sass --save-dev
+```
+
+
+## Running the tests
+
+To check that it has all been setup correctly. Once you have installed all the dependencies type **gulp** into the terminal.
+
+This will run all the required dependencies.
+
+Go to http://localhost:3000/ in your browser, you will now be able to see any changes you make to the files inside src folder.
+
+The gulp command will also create the **tmp** file which will create the development version of the contents of **src** folder.
+
+NOTE
+**ctrl c** will stop the watch and server
+
+If you are uploading the new site to a new repo and you want to remove the **tmp** and **dist** folders from the project you just need to type **gulp clean**  
+
+
+## Deployment
+
+To deploy the site run **gulp build** in terminal and it will create a **dist** folder inside the root folder where it will save all the production ready code. all the files will be minified.
+
+
+## Versioning
+
+**Version 1**
+First stable version of code
+
+
+## Authors
+
+**Joshua Winterbottom** - https://github.com/joshwb82
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
+## Acknowledgments
+
+This template was built in part by using the following sites:
+* https://hackernoon.com/how-to-automate-all-the-things-with-gulp-b21a3fc96885
+* https://css-tricks.com/gulp-for-beginners/  
+
---
+++ gulpfile.js
@@ -0,0 +1,146 @@
+/* ========================
+GULP REQUIRE FILES
+=========================== */
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var inject = require('gulp-inject');
+var htmlclean = require('gulp-htmlclean');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var del = require('del');
+
+
+
+/* ========================
+GULP PATHS
+=========================== */
+var paths = {
+
+  // source files
+  src: 'src/**/*',
+  srcHTML: 'src/**/*.html',
+  srcSCSS: 'src/**/*.scss',
+  srcJS: 'src/**/*.js',
+
+  // Development files
+  tmp: 'tmp',
+  tmpIndex: 'tmp/index.html',
+  tmpCSS: 'tmp/**/*.css',
+  tmpJS: 'tmp/**/*.js',
+
+  // Production files
+  dist: 'dist',
+  distIndex: 'dist/index.html',
+  distCSS: 'dist/**/*.css',
+  distJS: 'dist/**/*.js'
+};
+
+
+/* ========================
+GULP TASKS
+=========================== */
+
+
+// Tasks to create a development folder structure
+// -------------------------------------------------
+
+// Copys working html file to Dev enviroment
+gulp.task('html', function () {
+  return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
+});
+
+// Copys working css file to Dev enviroment
+gulp.task('css', function () {
+  return gulp.src(paths.srcSCSS).pipe(sass()).pipe(gulp.dest(paths.tmp));
+});
+
+// Copys working Javascript file to Dev enviroment
+gulp.task('js', function () {
+  return gulp.src(paths.srcJS).pipe(gulp.dest(paths.tmp));
+});
+
+// Runs above three tasks together
+gulp.task('copy', ['html', 'css', 'js']);
+
+// Adds relative links to all css and js files into the html file
+gulp.task('inject', ['copy'], function () {
+  var css = gulp.src(paths.tmpCSS);
+  var js = gulp.src(paths.tmpJS);
+  return gulp.src(paths.tmpIndex)
+    .pipe(inject( css, { relative:true } ))
+    .pipe(inject( js, { relative:true } ))
+    .pipe(gulp.dest(paths.tmp));
+});
+
+// ------------------------------------------------
+
+// Task creates a local server and then creates a dev enviroment
+gulp.task('serve', ['inject'], function () {
+  return gulp.src(paths.tmp)
+    .pipe(webserver({
+      port: 3000,
+      livereload: true
+    }));
+});
+
+// Task to watch for changes before serving them to the local server
+gulp.task('watch', ['serve'], function () {
+  gulp.watch(paths.src, ['inject']);
+});
+
+
+// Task to make watch the default task which allows you to just type gulp to run all tasks 
+gulp.task('default', ['watch']);
+
+
+// Tasks to create a production folder structure
+// -------------------------------------------------
+
+// Copys development html file to production enviroment
+gulp.task('html:dist', function () {
+  return gulp.src(paths.srcHTML)
+    .pipe(htmlclean())
+    .pipe(gulp.dest(paths.dist));
+});
+
+// Copys development css file to production enviroment
+gulp.task('css:dist', function () {
+  return gulp.src(paths.srcCSS)
+    .pipe(concat('style.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(paths.dist));
+});
+
+// Copys development javascript file to production enviroment
+gulp.task('js:dist', function () {
+  return gulp.src(paths.srcJS)
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist));
+});
+
+// Runs above three tasks together
+gulp.task('copy:dist', ['html:dist', 'css:dist', 'js:dist']);
+
+// Adds relative links to all css and js files into the html file
+gulp.task('inject:dist', ['copy:dist'], function () {
+  var css = gulp.src(paths.distCSS);
+  var js = gulp.src(paths.distJS);
+  return gulp.src(paths.distIndex)
+    .pipe(inject( css, { relative:true } ))
+    .pipe(inject( js, { relative:true } ))
+    .pipe(gulp.dest(paths.dist));
+});
+
+// ------------------------------------------------
+
+// task to create the build files 
+gulp.task('build', ['inject:dist']);
+
+// Task to clean the project file by remmoving the tmpa dn dist folders
+gulp.task('clean', function () {
+  del([paths.tmp, paths.dist]);
+});
+
